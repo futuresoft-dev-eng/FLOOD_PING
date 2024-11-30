a1090efc0b22635f23beda1e88f79850d272c608
eloni111 <144916052+eloni111@users.noreply.github.com>
2024-11-30 04:39:09 -0800

    Update admin_profile.php

--- admin_profile.php
+++ admin_profile.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'db_conn.php';
-include 'adminsidebar-accountservices.php';
+include 'adminsidebar.php';
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: ./HOME/login.php");
